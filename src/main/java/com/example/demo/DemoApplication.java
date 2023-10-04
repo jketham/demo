935471bd21121e4528f945dafe26b80a5ecdaf68
jketham <jketham@gmail.com>
2023-10-04 11:34:24 +0530

    added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("Welcome to Janardhana for demo ");
+		System.out.println("Welcome to Janardhana for demo sample");
 		
 		
 	}
